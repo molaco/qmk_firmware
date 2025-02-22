bd25b7f4c683e9133b8d8fb58a1e52f14fb5be62
molaco <hugo.cas2@icloud.com>
2025-02-22 21:42:25 +0000

    fix corne keymap

--- keyboards/crkbd/keymaps/molaco/keymap.c
+++ keyboards/crkbd/keymaps/molaco/keymap.c
@@ -200,7 +200,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //├────────┼────────┼────────┼────────┼────────┼────────┤      ├────────┼────────┼────────┼────────┼────────┼────────┤
     XXXXXXX ,KC_LALT ,XXXXXXX ,XXXXXXX ,KC_B    ,KC_M    ,       XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
  //└────────┴────────┼────────┼────┬───┴────┬───┼────────┐      ┌────────┼───┬────┴───┬────┼────────┼────────┴────────┘
-                       XXXXXXX,     _______ ,     XXXXXXX,        XXXXXXX,     XXXXXXX,      XXXXXXX,
+                       XXXXXXX,     _______ ,     XXXXXXX,        XXXXXXX,     XXXXXXX,      XXXXXXX
  //                  └────────┘    └────────┘   └────────┘      └────────┘   └────────┘    └────────┘
  ),
 };
